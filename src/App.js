411f580c1d18fc1e10b7c35ce0445a1a8b3c79a3
--replace <wathek.ghenimi1@gmail.com>
2021-08-14 15:38:25 +0400

    version 01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./Style.css";
+import imageInSrc from "./imageInSrc.jpg";
+import MyVideo from "./MyVideo.mp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+      <h1 className="title red">Wathek Ghenimi</h1>
+      <br />
+      <img src={imageInSrc} alt="imageInSrc" width="200px" height="200px" />
+      <br />
+      <img
+        src="/imageInPublic.jpg"
+        alt="imageInSrc"
+        width="200px"
+        height="200px"
+      />
+      <div>
+        <video width="320" height="240" controls>
+          <source src={MyVideo} type="video/mp4" />
+        </video>
+      </div>
     </div>
   );
 }
